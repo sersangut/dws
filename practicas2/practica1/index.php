d36b2879bf55e793225464d10d497c3b19dd2d4d
sergio <ssg@hotmail.com>
2021-03-16 11:20:20 +0100

    version 1

---
+++ practicas2/practica1/index.php
@@ -0,0 +1,51 @@
+﻿<!doctype html>
+<html lang="es">
+	<head>		
+		<meta charset="utf-8"/>
+		<title> Titulo </title>		
+	</head>	
+
+	<body>
+			
+	<?php
+		# Declaro las variables
+
+		$numero1=5;
+		$numero2=2;			
+	
+		echo "La variable \$numero1 tiene 
+						el valor <b> $numero1</b> <br/>";
+		
+		echo "La variable \$numero2 tiene el 
+					valor <b> $numero2</b> <br/>";
+	
+		echo "
+			<br/><br/>
+
+				<table border='1'>
+					<tr>
+						<th align='center'>Operación</th> 
+						<th align='center'>Resultado</th>
+					</tr>
+					<tr>
+						<td align='center'>\$numero1+\$numero2</td> 
+						<td align='center'>".($numero1+$numero2)."</td>
+					</tr>
+					<tr>
+						<td align='center'>\$numero1-\$numero2</td> 
+						<td align='center'>".($numero1-$numero2)."</td>
+					</tr>
+					<tr>
+						<td align='center'>\$numero1*\$numero2</td> 
+						<td align='center'>".($numero1*$numero2)."</td>
+					</tr>
+					<tr>
+						<td align='center'>\$numero1/\$numero2</td> 
+						<td align='center'>".($numero1/$numero2)."</td>
+					</tr>
+				</table>
+			";
+			
+			?>
+	</body>
+</html>
---
+++ practicas2/practica2/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="es">
+	<head>		
+		<meta charset="utf-8"/>
+		<title> Titulo </title>		
+	</head>	
+	<body>
+	<p align="center">
+			
+	<?php
+		# semilla de aleatorio
+		srand((double)microtime()*1000000);
+
+		# genero un aleatorio entre 1 y 6
+		$dado=rand(1,6);
+
+		switch($dado){
+			case 1:	echo "<img src='./img/1.png'>"; break;
+			case 2:	echo "<img src='./img/2.png'>"; break;
+			case 3:	echo "<img src='./img/3.png'>"; break;
+			case 4:	echo "<img src='./img/4.png'>"; break;
+			case 5:	echo "<img src='./img/5.png'>"; break;
+			case 6:	echo "<img src='./img/6.png'>"; break;					
+		}		
+	?>
+
+	<br/><br/>
+	<a href="index.php">Vuelve a tirar</a>
+	</p>
+	</body>
+</html>
